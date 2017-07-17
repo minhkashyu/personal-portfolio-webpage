e5c72583466f816d016943d85393f994ecc10835
minhkashyu <minhvinhta@yahoo.com>
2017-07-17 18:44:23 +1000

    collapse navbar-toggle after clicked

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-    'use strict';
     mtScroll('mt-home');
     mtScroll('mt-portfolio');
     mtScroll('mt-contact');
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+            $(this).collapse('hide');
+        }
+    });
 });
 
 var mtScroll = function (sectionID) {
     // animate the scroll when click on navbar
     $('a[href="#'+sectionID+'"]').click(function() {
         $('html, body').animate({
-            scrollTop: $('#'+sectionID).offset().top - 68
+            scrollTop: $('#'+sectionID).offset().top - 60
         }, 1000);
-        return false;
     });
 };
